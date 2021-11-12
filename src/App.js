aa48adf7134abe5d759dd575c371cb6b622feeac
Arafat <arafat97alim@gmail.com>
2021-11-12 15:34:30 +0530

    added sass and seperate component based architecture.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return (
-    <header className="header">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-xs-3 col-lg-2">
-            <div className="header__logo">
-              <font>E-Store</font>
-            </div>
-          </div>
-          <div className="col-xl-6 col-lg-7">
-            <div className="input-group">
-              <div className="input-group">
-                <input type="text" className="form-control" />
-              </div>
-              <div className="input-group-append">
-                <button className="btn btn-success">
-                  <i className="fa fa-search"></i>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+  return <Header />;
 }
 
 export default App;
